420334f2664e1a8b74eb5ba2688fd36d3980ac78
eugene0318 <nttott@gmail.com>
2023-09-21 02:27:07 +0900

    ~filter

--- login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ login/src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -1,21 +1,17 @@
 package hello.login.web.filter;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import hello.login.web.sessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Slf4j
 public class LoginCheckFilter implements Filter {
-
-	private static final String[] whiteList = { "/", "/member/add", "/login", "/logout", "/css/*" };
+	private static final String[] whitelist = { "/", "/members/add", "/login", "/logout", "/css/*" };
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -24,9 +20,29 @@ public class LoginCheckFilter implements Filter {
 		String requestURI = httpRequest.getRequestURI();
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
 		try {
-			log.info("인중 체크 필터 시작{}", requestURI);
+			log.info("인증 체크 필터 시작 {}", requestURI);
+			if (isLoginCheckPath(requestURI)) {
+				log.info("인증 체크 로직 실행 {}", requestURI);
+				HttpSession session = httpRequest.getSession(false);
+				if (session == null || session.getAttribute(sessionConst.LOGIN_MEMBER) == null) {
+					log.info("미인증 사용자 요청 {}", requestURI);
+					// 로그인으로 redirect
+					httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+					return; // 여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝!
+				}
+			}
+			chain.doFilter(request, response);
 		} catch (Exception e) {
-			// TODO: handle exception
+			throw e; // 예외 로깅 가능 하지만, 톰캣까지 예외를 보내주어야 함
+		} finally {
+			log.info("인증 체크 필터 종료 {}", requestURI);
 		}
+	}
+
+	/**
+	 * 화이트 리스트의 경우 인증 체크X
+	 */
+	private boolean isLoginCheckPath(String requestURI) {
+		return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
 	}
 }
